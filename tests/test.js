c5b0573e1f43b749f91af61770cd85d098ac3765
Ilya <admin@redguy.ru>
2021-12-26 23:04:40 +0300

    Fix memory leak

--- tests/test.js
+++ tests/test.js
@@ -1,24 +1,21 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 let Cache = require('..').Cache;
 let aCache = new Cache({
-    ttl: 600, loadStrategy:"multiple",
-    loader: async (key=[], payload) => {
-        console.log("loader call:" + key.map(key => JSON.stringify(key)).join(", "));
-        let res = [];
-        for (let keyElement of key) {
-            res.push({key:keyElement,value:keyElement.name.toLowerCase()});
-        }
-        return res;
+    ttl: 600, loadStrategy:"one",
+    loader: async (key="", payload) => {
+        await sleep(5000);
+        return key.toLowerCase();
     }
 });
 
 (async () => {
-    console.log(await aCache.mGet([{name:"BAN"}]));
-    //await aCache.get("BAN");
-    //aCache.delete({name:"BAN"});
-    //console.log(await aCache.get("LOL","LOLs"));
-    /*aCache.set("LOL", "lols");
-    console.log(await aCache.get("LOL"))
-    aCache.clear();
-    console.log(await aCache.get("LOL"))*/
-    console.log(aCache.stats());
+    let tasks = [];
+    for (let i = 0; i < 5000; i++) {
+        tasks.push(aCache.get("BBB"))
+    }
+    await Promise.all(tasks);
+    console.log(aCache.stats())
 })();
