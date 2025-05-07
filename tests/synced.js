ef610d4e6436a6b492e49b2c61a2cd8222ab2885
Ilya <admin@redguy.ru>
2025-05-07 19:31:20 +0300

    Fix RedisSyncedMemoryCache incorrect typecasting while syncing invalidation of value (number become string on other instances)

--- tests/synced.js
+++ tests/synced.js
@@ -9,6 +9,10 @@ let cacheB = new Redis("test", {redis: {host: "localhost", port: 6379}});
     await cacheB.set("A", "CC");
     await cacheB.delete("A");
     test(await cacheA.get("A") === null, "Delete check");
+    await cacheA.set(1, "number");
+    await cacheB.delete(1);
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    test(await cacheA.get(1) === null, "Deleting number value check");
 })();
 
 function test(assertion, message) {
